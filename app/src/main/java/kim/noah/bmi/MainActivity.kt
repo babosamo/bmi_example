6c6f8932b7323b92fc3684542de0b9b89ae3fc9d
noah <babosamo@gmail.com>
2018-11-27 17:53:57 +0900

    init submodule

--- app/src/main/java/kim/noah/bmi/MainActivity.kt
+++ app/src/main/java/kim/noah/bmi/MainActivity.kt
@@ -1,12 +1,14 @@
 package kim.noah.bmi
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import noah.kim.base.appContext
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        appContext = applicationContext
     }
 }
