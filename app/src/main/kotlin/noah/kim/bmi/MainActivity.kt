28ce6677b35a8885470249031e7301ec562c4724
noah <babosamo@gmail.com>
2018-11-28 17:00:11 +0900

    3장 click

--- app/src/main/kotlin/noah/kim/bmi/MainActivity.kt
+++ app/src/main/kotlin/noah/kim/bmi/MainActivity.kt
@@ -2,6 +2,7 @@ package noah.kim.bmi
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import noah.kim.base.appContext
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         appContext = applicationContext
+        clickButton.setOnClickListener{
+            textView.text = "버튼을 눌렀습니다"
+        }
     }
 }
